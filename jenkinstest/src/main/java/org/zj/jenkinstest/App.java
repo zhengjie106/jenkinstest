090d09a0c204e66cd1338be57af52a7316d8261b
Jack Zheng <jackzheng@JackMacBookPro.local>
2019-09-14 22:26:54 +0800

    sd

--- jenkinstest/src/main/java/org/zj/jenkinstest/App.java
+++ jenkinstest/src/main/java/org/zj/jenkinstest/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	for(int i=0 ; i < 10  ;i++) {
-        System.out.println( "Hello World! "+i );
+        System.out.println( "Hello World! --> "+i );
     	}
     }
 }
